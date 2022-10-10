e6435d1e1114511a25d8ce923164cfc29620b030
SamHamamji <samhamamji0@gmail.com>
2022-10-10 23:54:18 +0300

    Add documentation to Exercise 5

--- Assignment_1/more_arrays.c
+++ Assignment_1/more_arrays.c
@@ -18,9 +18,18 @@ void printStringArray(char **array, int size) {
   printf("}\n");
 }
 
+/**
+  @returns a 1D array containing the concatenation of elements in each element
+    of `array` with a space separation between each two elements
+  @expects:
+    The number of words in each row of `array` is the same
+    `size1` and `size2` are non-zero
+  @tested:
+    Empty arrays (`array1`, `array2`, both)
+*/
 char **concat(char *array[SENTENCES_NUMBER][MAX_WORD_LENGTH], unsigned int size,
               unsigned int size2) {
-  char **concatenation = (char **)calloc(size, size2);
+  char **concatenated = (char **)calloc(size, size2);
   char sentences[size][MAX_WORD_LENGTH * MAX_WORD_LENGTH / 2];
 
   for (int sentence_index = 0; sentence_index < size; sentence_index++) {
@@ -31,17 +40,17 @@ char **concat(char *array[SENTENCES_NUMBER][MAX_WORD_LENGTH], unsigned int size,
         strcat(sentences[sentence_index], " ");
       }
     }
-    concatenation[sentence_index] = sentences[sentence_index];
+    concatenated[sentence_index] = sentences[sentence_index];
   }
-  return concatenation;
+  return concatenated;
 }
 
 int main(void) {
   char *array[WORDS_NUMBER][MAX_WORD_LENGTH] = {{"I", "LOVE", "CODING", ":)"},
                                                 {"I", "am", "DONE", ":0"},
                                                 {"THIS", "IS", "SPARTA", "!"}};
-  char **concatenation = concat(array, SENTENCES_NUMBER, WORDS_NUMBER);
-  printStringArray(concatenation, SENTENCES_NUMBER);
-  free(concatenation);
+  char **concatenated = concat(array, SENTENCES_NUMBER, WORDS_NUMBER);
+  printStringArray(concatenated, SENTENCES_NUMBER);
+  free(concatenated);
   return 0;
 }
